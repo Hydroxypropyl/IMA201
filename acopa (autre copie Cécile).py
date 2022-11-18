c563fb8da9580f00e49e83f43ec88c2c6503b478
Hydroxyp <hydroxyp@gmail.com>
2022-11-19 00:02:35 +0100

    mise en commun codes (dans autre copie Cécile) et ajout images de test

--- acopa (autre copie Cécile).py
+++ acopa (autre copie Cécile).py
@@ -8,15 +8,65 @@ from sklearn.cluster import KMeans
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from sklearn.metrics import pairwise_distances_argmin
-from sklearn.datasets import load_sample_image
-from sklearn.utils import shuffle
-from time import time
-from skimage.io import imread
+
 # apply on hue histogram. 
 #link each pixel from grey cylinder to interval
 
 #%% convert image to a modified HSI
+
+
+def HSI_to_RGB_centers(colors : ArrayType) -> ArrayType :
+    """
+    Convert the centers from HSI to RGB.
+    
+    Parameters
+    -------------
+    `colors` : 
+    The colors in HSI
+    
+    Returns :
+    ---------------
+    `RGB_centers` :
+    The colors in RGB
+    """
+    print("entering HSI_to_RGB for centers...")
+    eps = 0.001
+    RGB_colors = []
+    for color in colors:
+        B = 0
+        G = 0
+        R = 0
+        H = color[0]
+        S = color[1]
+        I = color[2]
+
+        if np.abs(H) <= eps :
+            R = I + 2*I*S
+            G = I - I*S
+            B = I - I*S
+        elif np.abs(H-2*np.pi/3) < eps:
+            R = I - I*S
+            G = I + 2*I*S
+            B = I - I*S
+        elif np.abs(H + 2*np.pi/3) < eps : 
+            R = I - I*S
+            G = I - I*S
+            B = I + 2*I*S
+        elif eps < H and H < 2*np.pi/3  :
+            R = I + I*S*np.cos(H)/np.cos(np.pi/3 - H)
+            G = I + I*S*(1 - np.cos(H)/np.cos(np.pi/3 - H))
+            B = I - I*S
+        elif np.abs(H) > 2*np.pi/3 :
+            R = I - I*S
+            G = I + I*S*np.cos(H-2*np.pi/3)/np.cos(np.pi-H)
+            B = I + I*S*(1 - np.cos(H-2*np.pi/3)/np.cos(np.pi-H)) 
+        elif -2*np.pi/3 < H and H < eps : 
+            R = I + I*S*(1 - np.cos(H-4*np.pi/3)/np.cos(5*np.pi/3-H))
+            G = I - I*S
+            B = I + I*S*np.cos(H-4*np.pi/3)/np.cos(5*np.pi/3-H)
+        RGB_colors.append([math.floor(R),math.floor(G),math.floor(B)])
+    return RGB_colors
+
 
 def HSI_to_RGB(HSI_image : ArrayType) -> ArrayType :
     print("entering HSI_to_RGB...")
@@ -61,57 +111,16 @@ def HSI_to_RGB(HSI_image : ArrayType) -> ArrayType :
         RGB_image.append(row)
     return RGB_image
 
-def HSI_to_RGB_centers(centers : ArrayType) -> ArrayType :
-    print("entering HSI_to_RGB for centers...")
-    eps = 0.001
-    RGB_centers = []
-    for pixel in centers:
-        B = 0
-        G = 0
-        R = 0
-        H = pixel[0]
-        S = pixel[1]
-        I = pixel[2]
 
-        if np.abs(H) <= eps :
-            R = I + 2*I*S
-            G = I - I*S
-            B = I - I*S
-        elif np.abs(H-2*np.pi/3) < eps:
-            R = I - I*S
-            G = I + 2*I*S
-            B = I - I*S
-        elif np.abs(H + 2*np.pi/3) < eps : 
-            R = I - I*S
-            G = I - I*S
-            B = I + 2*I*S
-        elif eps < H and H < 2*np.pi/3  :
-            R = I + I*S*np.cos(H)/np.cos(np.pi/3 - H)
-            G = I + I*S*(1 - np.cos(H)/np.cos(np.pi/3 - H))
-            B = I - I*S
-        elif np.abs(H) > 2*np.pi/3 :
-            R = I - I*S
-            G = I + I*S*np.cos(H-2*np.pi/3)/np.cos(np.pi-H)
-            B = I + I*S*(1 - np.cos(H-2*np.pi/3)/np.cos(np.pi-H)) 
-        elif -2*np.pi/3 < H and H < eps : 
-            R = I + I*S*(1 - np.cos(H-4*np.pi/3)/np.cos(5*np.pi/3-H))
-            G = I - I*S
-            B = I + I*S*np.cos(H-4*np.pi/3)/np.cos(5*np.pi/3-H)
-        RGB_centers.append([math.floor(R),math.floor(G),math.floor(B)])
-    return RGB_centers
-
-
-def BGR_to_modified_HSI(BGR_image : ArrayType, saturation_threshhold : int = 0.08) -> ArrayType:
+def BGR_to_modified_HSI(BGR_image : ArrayType) -> ArrayType:
     """
-    Takes the image in BGR and returns the image where each pixel is coded by [H, S, I, 0, 0, 0]]
+    Takes the image in BGR and returns the image where each pixel is coded by [H, S, I, 0, 0, 0].
+    This coordinate system is used to indicate further on in which segmentation each of the pixel's color coordinates belong.
 
     Parameters:
     ---------------
     `image` :
     Initial image in BGR
-
-    `saturation_threshhold` :
-    Saturation threshhold under which a pixel is considered achromatic, so part of the grey cylinder
 
     Returns : 
     -------------
@@ -120,9 +129,10 @@ def BGR_to_modified_HSI(BGR_image : ArrayType, saturation_threshhold : int = 0.0
     """
     print("entering BGR_to_HSI...")
     HSI_image = np.zeros((BGR_image.shape[0], BGR_image.shape[1], 6))
-    for y in range(len(BGR_image)):
-        for x in range(len(BGR_image[y])): 
-            pixel = BGR_image[y][x]
+
+    for row_index in range(len(BGR_image)):
+        for column_index in range(len(BGR_image[row_index])): 
+            pixel = BGR_image[row_index][column_index]
             #récupérer les coordonnées RGB : opencv donne du BGR attention
             B = int(pixel[0])
             G = int(pixel[1])
@@ -132,13 +142,11 @@ def BGR_to_modified_HSI(BGR_image : ArrayType, saturation_threshhold : int = 0.0
                 S = 0
             else : 
                 S = 1 - np.min([B, G, R])/I #in [0, 1]
-            if True :
-                alpha = (2*R-G-B)/(2*255)
-                beta = np.sqrt(3)*(G-B)/(2*255)
-                H = np.arctan2(beta, alpha) # H is in [-pi, pi]
-                HSI_image[y][x] = [H, S, I, 0, 0, 0]
-            else :
-                HSI_image[y][x] = [-1, S, I, 0, 0, 0]
+
+            alpha = (2*R-G-B)/(2*255)
+            beta = np.sqrt(3)*(G-B)/(2*255)
+            H = np.arctan2(beta, alpha) # H is in [-pi, pi]
+            HSI_image[row_index][column_index] = [H, S, I, 0, 0, 0]
 
     return HSI_image
     
@@ -152,7 +160,8 @@ def get_hue_based_segmentation(HSI_image : ArrayType)  -> ArrayType :
 
     Parameters:
     ---------------
-    `image` : the image outputed by BGR_to_modified_HSI
+    `image` : 
+    the image outputed by BGR_to_modified_HSI
 
     Returns : 
     -------------
@@ -173,7 +182,7 @@ def get_hue_based_segmentation(HSI_image : ArrayType)  -> ArrayType :
             if pixel[1] >= (Q_hue+1)/(2*np.pi*255) : #condition of chromaticity
                 chromatic_hues.append(pixel[0])
     histo, bins = np.histogram(hues, bins = np.arange(-np.pi, np.pi, step=2*np.pi/(Q_hue+1)))
-    #H is better quantified, and is in [0, pi]
+    #H is better quantified, and is in [-pi, pi]
     chromatic_histo, bins = np.histogram(chromatic_hues, bins = np.arange(-np.pi, np.pi, step=2*np.pi/(Q_hue+1)))
     
     return FTC(chromatic_histo), histo
@@ -189,8 +198,12 @@ def get_saturation_based_segmentation(hue_segmentation : ArrayType, hue_histogra
     `hue_segmentation`:
     The hue segmentation of `HSI_image`
 
+    `hue_histogram` :
+    The hue histogram of `HSI_image`
+
     `HSI_image`:
-    An image in modified HSI just as BGR_to_modified_HSI_outputs (each pixel is [H, S, I, i, j, k])
+    The image in modified HSI just as BGR_to_modified_HSI_outputs (each pixel is [H, S, I, i, j, k]) where i,j,k indicate 
+    the index of the segmentation to which H, S and I respectively belong to.
 
     Returns : 
     -------------
@@ -204,6 +217,7 @@ def get_saturation_based_segmentation(hue_segmentation : ArrayType, hue_histogra
     [i] contains the  saturation histogram for pixels in S_i in hue_histogram 
    
     """
+
     print("entering get_saturation_based_segmentation...")
     # group pixels based on the hue segmentation they belong to :
     # saturation_grouped_by_hue[i] contains the pixels' saturation whose hues are in hue_segmentation[i]
@@ -215,7 +229,7 @@ def get_saturation_based_segmentation(hue_segmentation : ArrayType, hue_histogra
             pixel = HSI_image[line_index][row_index]
             hue_value = pixel[0]
             classified = False
-            #go through all the segments to find the one pixel belongs to, based on hue    
+            #go through all the segments to find the one the pixel belongs to, based on hue    
             i = 0
             while classified == False and i < len(hue_segmentation):
                 segment = hue_segmentation[i] #indices of start and end in hue_histogram
@@ -244,19 +258,34 @@ def get_saturation_based_segmentation(hue_segmentation : ArrayType, hue_histogra
 
 def get_intensity_based_segmentation(saturation_segmentation : ArrayType, saturation_histograms : ArrayType, HSI_image ) -> ArrayType :
     """
+    Calculates the intensity based segmentation of the different groups of pixesl defined by the segmentation segmentation
     
     Parameters:
     ---------------
     `saturation_segmentation`:
+    The saturation segmentation based on the hue segmentation, of `HSI_image`.
 
     `saturation_histograms` :
+    The saturation histograms based on the hue segmentation, of `HSI_image`.
 
+    `HSI_image` :
+    The image in modified HSI just as BGR_to_modified_HSI_outputs (each pixel is [H, S, I, i, j, k]) where i,j,k indicate 
+    the index of the segmentation to which H, S and I respectively belong to.
 
     Returns : 
     -------------
+    `intensity_segmentation` :
     The intensity_segmentation where intensity_segmentation[i][j] is the intensity segmentation for the pixels with
     their hue in hue_segmentation[i] and saturation_segmentation[i][j]. 
+
+    `HSI_image` :
+    updated image with the j and k components of pixels updated
+
+    `intensity_histograms` :
+    The intensity histograms
+
     """
+
     print("entering get_intensity_based_segmentation...")
     #create empty groups with same shape as saturation_segmentation
     intensity_grouped_by_saturation = [[] for i in range(len(saturation_segmentation))]
@@ -281,12 +310,11 @@ def get_intensity_based_segmentation(saturation_segmentation : ArrayType, satura
                     for j in range(len(segment)) :
                         if j== 0:
                             min = segment[j][0]/Q_sat
-                        else : 
+                        else : # start from the end of the previous segment to not have a discontinuity. Could be improved
                             min = segment[j-1][-1]/Q_sat
                         max = segment[j][-1]/Q_sat
                         if j == len(segment)-1 :
                             max = (segment[j][-1] + 1) /Q_sat
-                        #print("interval :", min, saturation_value, max)
                         if saturation_value >= min and saturation_value <= max : 
                             intensity_grouped_by_saturation[i][j].append(pixel[2])
                             HSI_image[line_index][row_index][4] = j
@@ -302,7 +330,6 @@ def get_intensity_based_segmentation(saturation_segmentation : ArrayType, satura
         S_i = intensity_grouped_by_saturation[i]
         for j in range(len(S_i)) :
             S_ij = S_i[j]
-            #print("S_" +str(i) +"," +str(j) +" : ", S_ij)
             histo, bins = np.histogram(S_ij, bins = np.arange(0, 255, 255/(Q_int+1)))
             intensity_segmentation[i][j] = FTC(histo)
             intensity_histograms[i][j] = histo
@@ -324,6 +351,23 @@ def get_intensity_based_segmentation(saturation_segmentation : ArrayType, satura
 
 #%% Mode computing functions
 def compute_hue_modes(hue_segmentation : ArrayType, hue_histogram : ArrayType) -> ArrayType : 
+    """
+    Computes the modes of each of the segments of the hue histogram.
+    
+    Parameters
+    -----------------
+    `hue_segmentation`:
+    The hue segmentation
+    
+    `hue_histogram`:
+    The hue histogram
+
+    Returns :
+    -----------------
+    `hue modes`:
+    Array of same shape as `hue segmentation` where each segment contains one value : the mode for the segment
+    """
+
     print("entering computing_hue_modes...")
     hue_modes = []
     for S_i in hue_segmentation:
@@ -332,7 +376,25 @@ def compute_hue_modes(hue_segmentation : ArrayType, hue_histogram : ArrayType) -
         hue_modes.append(-np.pi + max_index*2*np.pi/Q_hue)
     return hue_modes
 
+
 def compute_saturation_modes(saturation_segmentation : ArrayType, saturation_histograms : ArrayType) -> ArrayType : 
+    """
+    Computes the modes of each of the segments of the saturation histogram.
+    
+    Parameters
+    -----------------
+    `saturation_segmentation`: array
+    The saturation segmentation
+    
+    `saturation_histogram`:array
+    The saturation histogram
+
+    Returns :
+    -----------------
+    `saturation modes`: array
+    Array of same shape as `saturation segmentation` where each segment contains one value : the mode for the segment
+    """
+
     print("entering computing_saturation_modes...")
     saturation_modes = []
     for i in range(len(saturation_segmentation)) : 
@@ -346,8 +408,11 @@ def compute_saturation_modes(saturation_segmentation : ArrayType, saturation_his
         saturation_modes.append(sub_seg)
     return saturation_modes
 
+
 def compute_intensity_modes(intensity_segmentation : ArrayType, intensity_histograms : ArrayType) -> ArrayType : 
     """
+    Computes the modes of each of the segments of the intensity histogram.
+
     Parameters : 
     ---------------
     `intensity_segmentation` : array 
@@ -364,6 +429,7 @@ def compute_intensity_modes(intensity_segmentation : ArrayType, intensity_histog
     `nb_modes` : int 
     The total number of modes of the intensity component 
     """
+
     print("entering computing_intensity_modes...")
     intensity_modes = []
     nb_modes = 0
@@ -389,20 +455,19 @@ def acopa(image : ArrayType, HSI : bool = False) -> ArrayType:
     Applies the acopa algorithm on `image`. 
     By default, `image` must be in BGR and is converted in the function.
     If `HSI` = True then the `image` is considered already in HSI and acopa is applied without any conversion.
+
     Parameters:
     --------------
     `image` : array
     The image to apply the acopa algorithm
 
     `HSI` : bool
-    Whether the image is coded in HSI or not. 
+    Whether the image is coded in HSI or not.
+
     Returns : 
     ---------
-    `new_image` : array
-    The color coded image, before applying k-means
-    
-    `centers` : array
-    The centers that we are going to use for k-means
+    `colors` : array
+    The colors in RGB that we are going to use for k-means
     
     `nb_colors` : int
     The number of centers (colors) found after the different FTC algorithms
@@ -418,80 +483,49 @@ def acopa(image : ArrayType, HSI : bool = False) -> ArrayType:
     hue_modes = compute_hue_modes(hue_segmentation, hue_histogram)
     saturation_modes = compute_saturation_modes(saturation_segmentation, saturation_histograms)
     intensity_modes, nb_colors = compute_intensity_modes(intensity_segmentation, intensity_histograms)
-    new_image = np.zeros(image.shape)
     
-    centers = []
-    registered = []
-    for row_index in range(len(HSI_image)):
-        for column_index in range(len(HSI_image[row_index])):
-            pixel = HSI_image[row_index][column_index]
-            i = math.floor(pixel[3])
-            j = math.floor(pixel[4])
-            k = math.floor(pixel[5])
-            if [i,j,k] not in registered:
-                registered.append([i,j,k])
-                centers.append([hue_modes[i][0],saturation_modes[i][j][0],intensity_modes[i][j][k][0]])
-            new_image[row_index][column_index][0] = hue_modes[i][0]
-            new_image[row_index][column_index][1] = saturation_modes[i][j][0]
-            new_image[row_index][column_index][2] = intensity_modes[i][j][k][0]
-                
-    return new_image, np.asarray(centers), nb_colors
-    
-def kmeans_end(image : ArrayType, HSI : bool = False) -> ArrayType:
-    """
-    Applies the acopa algorithm on `image`. 
-    By default, `image` must be in BGR and is converted in the function.
-    If `HSI` = True then the `image` is considered already in HSI and acopa is applied without any conversion.
-    Parameters:
-    --------------
-    `image` : array
-    The image to apply k-means on
 
-    `HSI` : bool
-    Whether the image is coded in HSI or not
-    
-    Returns : 
-    ---------
-    `new_image` : array
-    The desired image
-    
-    `nb_colors` : int
-    The number of centers (colors) found for the k-means algorithm
-    """
-    
-    print("entering acopa...")
-    first_paint, centers, nb_colors = acopa(image, HSI)
-    centers_RGB = np.asarray(HSI_to_RGB_centers(centers))
-    if HSI:
-        image = np.asarray(HSI_to_RGB(image))
-    #from here, everything is in RGB
-    w, h, d = tuple(image.shape)
-    assert d == 3
-    image_array = np.reshape(image, (w * h, d))
-    image_array_sample = shuffle(image_array, random_state=0)[:1000]
-    kmeans = KMeans(n_clusters=nb_colors, init=centers_RGB, n_init=nb_colors).fit(image_array_sample)
-    real_centers = kmeans.cluster_centers_
-    labels = kmeans.predict(image_array)
-    d = real_centers.shape[1]
-    new_image = np.zeros((w, h, d))
-    label_idx = 0
-    for i in range(w):
-        for j in range(h):
-            new_image[i][j] = real_centers[labels[label_idx]]
-            label_idx += 1
-    return new_image.astype(int), nb_colors
+    colors = []
+    for i in range(len(hue_modes)) :
+        H = hue_modes[i][0]
+        for j in range(len(saturation_modes[i])) :
+            S = saturation_modes[i][j][0]
+            for k in range(len(intensity_modes[i][j])):
+                I = intensity_modes[i][j][k][0]
+                colors.append([H, S, I])
+
+    return HSI_to_RGB_centers(colors), nb_colors
+
+            
+def final_palette(img, seeds): 
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    w, h, d = img.shape
+    img_array = np.reshape(img, (w * h, d))
+    kmeans = KMeans(n_clusters = len(seeds), init = seeds).fit(img_array)
+    rgb_colors = kmeans.cluster_centers_.round(0).astype(int)
+    labels = kmeans.fit_predict(img_array)
+    img_quant = np.reshape(rgb_colors[labels],(w,h,d))
+    labels = list(labels)
+    percent = []
+    for i in range(len(rgb_colors)):
+        j=labels.count(i)
+        j=j/(len(labels))
+        percent.append(j)
+    return img_quant, rgb_colors, percent
 
 #%% Test
-image = cv2.imread("carottes.jpg")
+image = cv2.imread("low_luminosity.png")
 HSI_image = BGR_to_modified_HSI(image)
 Q_sat = 180
-Q_int = 200
+Q_int = 500
 Q_hue = 80
-img2, centers, nb_colors = acopa(HSI_image, True)
-img2 = HSI_to_RGB(img2)
-img1, nb_colors = kmeans_end(HSI_image, True)
-plt.imshow(img2)
+colors, nb_colors = acopa(HSI_image, True)
+colors = HSI_to_RGB_centers(colors)
+img1, RGB_colors, percent = final_palette(image, colors)
+plt.imshow(img1)
 plt.title(label = "Q_int = " +str(Q_int) +" Q_sat = " + str(Q_sat) + " Q_hue = "+ str(Q_hue) + ", nb colors = " +str(nb_colors))
+fig = plt.figure()
+plt.pie(percent,colors=np.array(RGB_colors/255),labels=np.arange(len(RGB_colors)))
 plt.show()
 
 
